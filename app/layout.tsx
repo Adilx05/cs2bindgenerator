f62f829766674ad182215a8f32fc9d2e4d3f5365
Adil <adilx05@gmail.com>
2026-05-31 14:02:21 +0300

    Added google anlytics tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,19 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-G3LYDSF8RT" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-G3LYDSF8RT');
+            `
+          }}
+        />
+      </head>
       <body className={inter.className}>
         <Navbar />
         <main className="mx-auto max-w-7xl px-4 py-8">{children}</main>
